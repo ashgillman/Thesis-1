b0c51814ec52b54581734fb7b3f5c1f48133e967
TheMerovingian <bryan.quill@gmail.com>
2014-03-24 12:53:21 +1000

    Moved the command line test code to the SystemTest script and cleared the FileConversion script to contain only directory information.

--- Scripts/FileConversion.py
+++ Scripts/FileConversion.py
@@ -3,6 +3,8 @@ from os import environ
 from platform import system as checkOS
 
 HOME = environ['HOME']
+CLEAN_AUDIO_DIR = "/Default"
+CONVERT_OUTPUT_DIR = "/Converted"
 
 # Check whether this is running on my mac or windows machine
 if checkOS() == "Darwin":
@@ -12,15 +14,6 @@ else:
 
 OPERATING_DIR = str.format("{0}{1}", HOME, THESIS_DIR)
 
-CLEAN_AUDIO_DIR = "/Default"
-CONVERT_OUTPUT_DIR = "/Converted"
-
-results = check_output(["ls", OPERATING_DIR + CLEAN_AUDIO_DIR])
-
-results = "/" + results.decode("UTF-8").strip("\n")
 
 
-result2 = check_output(["mv",
-                        OPERATING_DIR + CLEAN_AUDIO_DIR + results,
-                        OPERATING_DIR + CONVERT_OUTPUT_DIR + results])
 
--- Scripts/SystemTest.py
+++ Scripts/SystemTest.py
@@ -1,11 +1,25 @@
 from subprocess import call, check_output, check_call
 from os import environ
+from platform import system as checkOS
 
-TEST_DIR = environ['HOME'] + "/Dropbox"
+HOME = environ['HOME']
 
-check_output(["ls", "-a", "../.."])
+# Check whether this is running on my mac or windows machine
+if checkOS() == "Darwin":
+    THESIS_DIR = "/Dropbox/Thesis/Audio"
+else:
+    THESIS_DIR = ""
 
-results = check_output(["ls", TEST_DIR])
+OPERATING_DIR = str.format("{0}{1}", HOME, THESIS_DIR)
 
-print(TEST_DIR)
-print(results.split(b"\n"))
+CLEAN_AUDIO_DIR = "/Default"
+CONVERT_OUTPUT_DIR = "/Converted"
+
+results = check_output(["ls", OPERATING_DIR + CLEAN_AUDIO_DIR])
+
+results = "/" + results.decode("UTF-8").strip("\n")
+
+
+result2 = check_output(["mv",
+                        OPERATING_DIR + CLEAN_AUDIO_DIR + results,
+                        OPERATING_DIR + CONVERT_OUTPUT_DIR + results])
