1ca3d336943ad25cf9cb826ce38f59c7b3122211
TheMerovingian <bryan.quill@gmail.com>
2014-03-24 11:54:38 +1000

    Tested command line execution to move a file within the file system.

--- Scripts/FileConversion.py
+++ Scripts/FileConversion.py
@@ -6,10 +6,21 @@ HOME = environ['HOME']
 
 # Check whether this is running on my mac or windows machine
 if checkOS() == "Darwin":
-    THESIS_DIR = "/Dropbox/Thesis"
+    THESIS_DIR = "/Dropbox/Thesis/Audio"
 else:
     THESIS_DIR = ""
 
 OPERATING_DIR = str.format("{0}{1}", HOME, THESIS_DIR)
 
-results = check_output(["ls", OPERATING_DIR])
+CLEAN_AUDIO_DIR = "/Default"
+CONVERT_OUTPUT_DIR = "/Converted"
+
+results = check_output(["ls", OPERATING_DIR + CLEAN_AUDIO_DIR])
+
+results = "/" + results.decode("UTF-8").strip("\n")
+
+
+result2 = check_output(["mv",
+                        OPERATING_DIR + CLEAN_AUDIO_DIR + results,
+                        OPERATING_DIR + CONVERT_OUTPUT_DIR + results])
+
