c81d9711dd1cbfb7b7913ff3c8cce2642e043378
TheMerovingian <bryan.quill@gmail.com>
2014-03-27 23:46:22 +1000

    Completed conversion script. Converts the .wp1/2 files and copies across the complementary pheonetic and word files.

--- Scripts/ConvertTest.sh
+++ Scripts/ConvertTest.sh
@@ -10,6 +10,16 @@ echo "Directory containing the audio data to be converted is \n$OPERATING_DIR"
 
 for f in $FILES
 do
+
+    # Grab the underlying directory structure for the data
+    dirStruct=${f:$OPERATING_SIZE}
+    length=${#dirStruct}
+    length=$((length-13))
+    dirStruct=${dirStruct:0:length}
+
+    # Create the directory structure for the output data
+    mkdir -p "$OUTPUT$dirStruct"
+
     # Find all files within the FILES directory that end with .wv1/2
 	name=$(find "$f" | egrep -i -o -e .*\.wv[1,2])
 
@@ -20,9 +30,12 @@ do
     size=$((size-4))
 
     # Check whether or not the length is > 0
-    # Length < 0 represents no matching file
+    # Length < 0 represents no .wv* extension
+    # Copy all the pheonetic/word data into the conversion folders
     if [ $size -le 0 ]
     then
+        copyfile=${f:$OPERATING_SIZE}
+        cp -f "$f" "$OUTPUT$copyfile"
         continue
     fi
 
@@ -33,7 +46,7 @@ do
     name=${name:$OPERATING_SIZE}
 
     # Run the .wv* to .wav conversion
-    sph2pipe -p -f rif "$f" "$OUTPUT/$name.wav"
+    sph2pipe -p -f rif "$f" "$OUTPUT$name.wav"
 
 done
 
