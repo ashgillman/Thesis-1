3abe32718a2d53a7495432f48984813429682eeb
TheMerovingian <bryan.quill@gmail.com>
2014-08-28 13:03:14 +1000

    Wrote hmmDef and macro creation functions

--- Scripts/ListCreation.py
+++ Scripts/ListCreation.py
@@ -27,6 +27,8 @@ SCRIPT_EXT = ".scp"
 # Master Label File Extension
 MLF_EXT = ".mlf"
 
+# Phoneme Constants
+SILENCE_PHN = "sil"
 
 def listAllFiles(dir, ext):
     return [name for name in [f for r,d,f in os.walk(dir)][0] if ext in name]
@@ -46,88 +48,134 @@ def createLabFiles():
                 shutil.copyfile(phnFile, labFile)
 
 
-
-
-createLabFiles()
-
-
 """ Generate WAV -> MFC list """
 
-# Clear WAV -> MFC conversion file, to allow appending
-wavConvertFile = str.format("{0}{1}{2}", OUTPUT_DIR, "WAV_MFC_Conversion_List", SCRIPT_EXT)
-open(wavConvertFile, 'w').close()
+def generateConversionList():
+    # Clear WAV -> MFC conversion file, to allow appending
+    wavConvertFile = str.format("{0}{1}{2}", OUTPUT_DIR, "WAV_MFC_Conversion_List", SCRIPT_EXT)
+    open(wavConvertFile, 'w').close()
 
-for folder in os.listdir(TRAINING_AUDIO_DIR):
+    for folder in os.listdir(TRAINING_AUDIO_DIR):
 
-    dir = str.format("{0}{1}\\", TRAINING_AUDIO_DIR, folder)
-    files = listAllFiles(dir, AUDIO_EXT)
+        dir = str.format("{0}{1}\\", TRAINING_AUDIO_DIR, folder)
+        files = listAllFiles(dir, AUDIO_EXT)
 
-    fid = open(wavConvertFile, 'a+')
+        fid = open(wavConvertFile, 'a+')
 
-    for file in files:
-        [name, _] = file.split('.')
+        for file in files:
+            [name, _] = file.split('.')
 
-        output = str.format("{0}{1} {2}{3}{4}",
-                    dir,
-                    file,
-                    CLASSIFIER_DIR,
-                    name,
-                    MFC_EXT)
+            output = str.format("{0}{1} {2}{3}{4}",
+                        dir,
+                        file,
+                        CLASSIFIER_DIR,
+                        name,
+                        MFC_EXT)
 
-        fid.write(output + "\n")
+            fid.write(output + "\n")
 
-    fid.close()
+        fid.close()
 
 
 """ Classifier Training Lists """
+def generateClassifierLists():
+    for ext in CLASSIFIER_EXTS:
 
-for ext in CLASSIFIER_EXTS:
+        listFile = str.format("{0}{1}_Training_List.scp", OUTPUT_DIR, ext.lstrip('.'))
 
-    listFile = str.format("{0}{1}_Training_List.scp", OUTPUT_DIR, ext.lstrip('.'))
+        files = listAllFiles(CLASSIFIER_DIR, ext)
 
-    files = listAllFiles(CLASSIFIER_DIR, ext)
+        fid = open(listFile, 'w')
 
-    fid = open(listFile, 'w')
+        for file in files:
+            fid.write(str.format("{0}{1}\n", CLASSIFIER_DIR, file))
 
-    for file in files:
-        fid.write(str.format("{0}{1}\n", CLASSIFIER_DIR, file))
-
-    fid.close()
+        fid.close()
 
 
 """ Master Label File Creation """
+def generateMLF():
+    MLFFile = str.format("{0}{1}{2}", OUTPUT_DIR, "phones0", MLF_EXT)
 
-MLFFile = str.format("{0}{1}{2}", OUTPUT_DIR, "phones0", MLF_EXT)
+    # Overwrite the MLF file
+    fid = open(MLFFile, 'w')
+    fid.write("#!MLF!#\n")
+    fid.close()
 
-# Overwrite the MLF file
-fid = open(MLFFile, 'w')
-fid.write("#!MLF!#\n")
-fid.close()
+    for folder in os.listdir(TRAINING_AUDIO_DIR):
 
-for folder in os.listdir(TRAINING_AUDIO_DIR):
+        dir = str.format("{0}{1}\\", TRAINING_AUDIO_DIR, folder)
+        files = listAllFiles(dir, PHONEME_EXT.upper())
 
-    dir = str.format("{0}{1}\\", TRAINING_AUDIO_DIR, folder)
-    files = listAllFiles(dir, PHONEME_EXT.upper())
+        for file in files:
 
-    for file in files:
+            label = str.format("\"*/{0}\"", file)
 
-        label = str.format("\"*/{0}\"", file)
+            phn = open(dir + file, 'r+')
 
-        phn = open(dir + file, 'r+')
+            mlf = open(MLFFile, 'a+')
 
-        mlf = open(MLFFile, 'a+')
+            mlf.write(label + "\n")
 
-        mlf.write(label + "\n")
+            for line in phn:
+                mlf.write(line)
 
-        for line in phn:
-            mlf.write(line)
+            mlf.write(".\n")
 
-        mlf.write(".\n")
+            phn.close()
+            mlf.close()
 
-        phn.close()
-        mlf.close()
+def generateHMMDefs():
+    phonemeList = str.format("{0}{1}{2}", OUTPUT_DIR, "phones0", MLF_EXT) #TODO: Set proper PhonemeList location
+    hmmDefs = str.format("{0}{1}{2}", OUTPUT_DIR, "phones0", MLF_EXT) #TODO: Set proper HMMDef location
+    proto = str.format("{0}{1}{2}", OUTPUT_DIR, "phones0", MLF_EXT) #TODO: Set proper proto location
 
+    phn = open(phonemeList, 'r')
+    hmm = open(hmmDefs, 'w')
 
+    for line in phn:
+        hmm.write(str.format("~h {0}", line))
 
+        if line.lower() == SILENCE_PHN:
+            hmm.write("\n")
 
+        protoFile = open(proto, 'r')
+        lineCount = 0
 
+        for protoLine in protoFile:
+
+            if lineCount > 3:
+                hmm.write(protoLine)
+
+            lineCount += 1
+
+        hmm.write("\n")
+
+        proto.close()
+
+    phn.close()
+    hmm.close()
+
+def generateMacros():
+    macros = str.format("{0}{1}{2}", OUTPUT_DIR, "phones0", MLF_EXT) #TODO: Set proper Macros location
+    macroHeaders = str.format("{0}{1}{2}", OUTPUT_DIR, "phones0", MLF_EXT) #TODO: Set proper MacroHeaders location
+    vFloor = str.format("{0}{1}{2}", OUTPUT_DIR, "phones0", MLF_EXT) #TODO: Set proper vFloor location
+
+    macroFile = open(macros, 'w')
+    headerFile = open(macroHeaders, 'r')
+
+    for line in headerFile:
+        macroFile.write(line)
+
+    macroFile.write("\n")
+
+    headerFile.close()
+
+    vFloorFile = open(vFloor, 'w')
+
+    for line in vFloorFile:
+        macroFile.write(line)
+
+    vFloorFile.close()
+
+    macroFile.close()
