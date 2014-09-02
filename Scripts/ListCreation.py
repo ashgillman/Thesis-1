17d939f307103d79875410a6c96c5fe0c933416a
TheMerovingian <bryan.quill@gmail.com>
2014-09-02 20:03:29 +1000

    Working (pre-dictionary deletion)

--- Scripts/ListCreation.py
+++ Scripts/ListCreation.py
@@ -6,7 +6,7 @@ from time import sleep
 
 """ Define System Directory Constants """
 if platform.system() == "Windows":
-    FILE_START = "F:\\Thesis\\External\\"
+    FILE_START = "J:\\"
     SEPARATOR = "\\"
 else:
     FILE_START = "/Volumes/External/"
@@ -15,6 +15,7 @@ else:
 # Directory Constants
 TRAINING_DIR = str.format("{0}ClassifierTraining{1}", FILE_START, SEPARATOR)
 TRAINING_AUDIO_DIR = str.format("{0}ConvertData{1}ThesisData{1}Desk{1}Testing{1}Development{1}", FILE_START, SEPARATOR)
+EVAL_AUDIO_DIR = str.format("{0}ConvertData{1}ThesisData{1}Desk{1}Testing{1}Evaluation{1}", FILE_START, SEPARATOR)
 SCRIPT_DIR = str.format("{0}Lists{1}", TRAINING_DIR, SEPARATOR)
 CLASSIFIER_DIR = str.format("{0}Classifiers{1}", TRAINING_DIR, SEPARATOR)
 DATA_DIR = str.format("{0}ConvertData{1}", FILE_START, SEPARATOR)
@@ -57,16 +58,15 @@ PHONE_WITH_SP = PHONE_LOC + '1'
 SLEEP_S = 3
 
 # Number of training iterations
-TRAINING_COUNT = 9
+TRAINING_COUNT = 15
 
 # Dictionary Location
-DICT_LOC = TRAINING_DIR + "Training" + SEPARATOR + "phonedict"
+DICT_LOC = TRAINING_DIR + "Training" + SEPARATOR + "OnlineDict"
 
 def listAllFiles(dir, ext):
     return [name for name in [f for r,d,f in os.walk(dir)][0] if name.lower().endswith(ext.lower())]
 
 def buildFileStructure():
-
     for ext in CLASSIFIER_EXTS:
         ext = ext.lstrip('.').upper()
         dir = str.format("{0}{1}", HMM_DIR, ext)
@@ -77,6 +77,72 @@ def buildFileStructure():
             subdir = str.format("{0}{1}hmm{2}", dir, SEPARATOR, i)
             if (not os.path.exists(subdir)):
                 os.mkdir(subdir)
+
+def buildWordList():
+    for root, subdirs, files in os.walk(TRAINING_AUDIO_DIR):
+
+        phones = []
+
+        if os.path.isfile(DICT_LOC):
+            open(DICT_LOC, 'w').close()
+
+        for file in files:
+            [name, ext] = file.split(".")
+
+            if ext.lower() in PHONEME_EXT.lower():
+                phnFile = os.path.join(root, file)
+                wrdFile = os.path.join(root, name + WRD_EXT.upper())
+
+                if os.path.isfile(phnFile) and os.path.isfile(wrdFile):
+                    phones = transcribeFiles(wrdFile, phnFile, phones)
+
+def transcribeFiles(wrdFile, phnFile, existingDict=[]):
+    phn = open(phnFile, 'r')
+    wrd = open(wrdFile, 'r')
+
+    dict = open(DICT_LOC, 'a+')
+
+    for line in wrd:
+        [start, end, word] = line.strip("\n").split("\t")
+
+        start = int(start)
+        end = int(end)
+        word = word.strip('.')
+
+        if word in existingDict:
+            [_, phnEnd, _] = phn.readline().strip("\n").split("\t")
+            while (int(phnEnd) < end):
+                [_, phnEnd, _] = phn.readline().strip("\n").split("\t")
+            continue
+
+        dict.write(word.ljust(20))
+        existingDict.append(word)
+
+        [phnStart, phnEnd, phone] = phn.readline().strip("\n").split("\t")
+
+        phnStart = int(phnStart)
+        phnEnd = int(phnEnd)
+
+        while(phnStart < start):
+            phnLine = phn.readline().strip("\n")
+
+            [phnStart, phnEnd, phone] = phnLine.split("\t")
+            phnStart = int(phnStart)
+            phnEnd = int(phnEnd)
+
+        while(phnEnd < end):
+            dict.write(phone + " ")
+            phnLine = phn.readline().strip("\n")
+            [_, phnEnd, phone] = phnLine.split("\t")
+            phnEnd = int(phnEnd)
+
+        dict.write(phone + "\n")
+
+    phn.close()
+    wrd.close()
+    dict.close()
+
+    return existingDict
 
 def createLabFiles():
     """
@@ -94,14 +160,13 @@ def createLabFiles():
 
 
 """ Generate WAV -> MFC list """
-def generateConversionList():
+def generateConversionList(audioDir, outputFile, eval=False):
     # Clear WAV -> MFC conversion file, to allow appending
-    wavConvertFile = str.format("{0}{1}{2}", SCRIPT_DIR, "WAV_MFC_Conversion_List", SCRIPT_EXT)
-    open(wavConvertFile, 'w').close()
+    open(outputFile, 'w').close()
 
-    for folder in os.listdir(TRAINING_AUDIO_DIR):
+    for folder in os.listdir(audioDir):
 
-        dir = str.format("{0}{1}{2}", TRAINING_AUDIO_DIR, folder, SEPARATOR)
+        dir = str.format("{0}{1}{2}", audioDir, folder, SEPARATOR)
         files = listAllFiles(dir, AUDIO_EXT)
 
         fid = open(wavConvertFile, 'a+')
@@ -109,10 +174,15 @@ def generateConversionList():
         for file in files:
             [name, _] = file.split('.')
 
-            output = str.format("{0}{1} {2}{3}{4}",
+            folder = "Training" + SEPARATOR
+            if eval:
+                folder = "Eval" + SEPARATOR
+
+            output = str.format("{0}{1} {2}{3}{4}{5}",
                         dir,
                         file,
                         CLASSIFIER_DIR,
+                        folder,
                         name,
                         MFC_EXT)
 
@@ -122,33 +192,29 @@ def generateConversionList():
 
 
 """ Classifier Training Lists """
-def generateClassifierLists():
-    for ext in CLASSIFIER_EXTS:
-        ext = ext.lstrip('.').upper()
-        listFile = str.format("{0}{1}_Training_List.scp", SCRIPT_DIR, ext)
+def generateClassifierLists(audioDir, outputFile):
 
-        files = listAllFiles(CLASSIFIER_DIR, ext)
+    files = listAllFiles(CLASSIFIER_DIR, ext)
 
-        fid = open(listFile, 'w')
+    fid = open(outputFile, 'w')
 
-        for file in files:
-            fid.write(str.format("{0}{1}\n", CLASSIFIER_DIR, file))
+    for file in files:
+        fid.write(str.format("{0}{1}\n", CLASSIFIER_DIR, file))
 
-        fid.close()
+    fid.close()
 
 
 """ Master Label File Creation """
-def generateMLF():
-    MLFFile = str.format("{0}{1}{2}", SCRIPT_DIR, "phones0", MLF_EXT)
+def generateMLF(audioDir, MLFFile):
 
     # Overwrite the MLF file
     fid = open(MLFFile, 'w')
     fid.write("#!MLF!#\n")
     fid.close()
 
-    for folder in os.listdir(TRAINING_AUDIO_DIR):
+    for folder in os.listdir(audioDir):
 
-        dir = str.format("{0}{1}{2}", TRAINING_AUDIO_DIR, folder, SEPARATOR)
+        dir = str.format("{0}{1}{2}", audioDir, folder, SEPARATOR)
         files = listAllFiles(dir, PHONEME_EXT.upper())
 
         for file in files:
@@ -299,8 +365,6 @@ def createMyOwnFuckingDictionary():
         for file in files:
             [name, ext] = file.split(".")
 
-            print(name + "\t" + ext)
-
             if ext.lower() in PHONEME_EXT.lower():
                 phnFile = os.path.join(root, file)
                 wrdFile = os.path.join(root, name + WRD_EXT.upper())
@@ -394,15 +458,16 @@ def performRealignment(ext, currentIteration):
     macros = str.format("{0}{1}{2}hmm{3}{2}{1}Macros", HMM_DIR, ext, SEPARATOR, currentIteration)
     hmmDef = str.format("{0}{1}{2}hmm{3}{2}HMMDef", HMM_DIR, ext, SEPARATOR, currentIteration)
 
-    command = str.format("HVite -l '*' -o SWT -b SIL -C {0} -a -H {1} -H {2} -i {3} -m -t 250.0 -y lab -I {4} -S {5} {6} {7}",
+    command = str.format("HVite -l '*' -o SWT -b SIL -C {0} -a -H {1} -H {2} -i {3} -m -t 250.0 -y lab -I {4} -S {5} {6} {7} > {8}",
                          HCOMPV_CONFIG.replace("||", ext),
                          macros,
                          hmmDef,
                          SCRIPT_DIR + ext + "NewPhones0.mlf",
                          SCRIPT_DIR + "phones0.mlf",
                          SCRIPT_DIR + ext + "_Training_List.scp",
-                         DICT_LOC, # TODO: Figure out dict file
-                         PHONE_WITH_SP
+                         DICT_LOC,
+                         PHONE_WITH_SP,
+                         TRAINING_DIR + "HVITE.log"
                          )
 
     os.system(command)
@@ -414,7 +479,8 @@ while (not command.startswith("Q")):
     if (command.startswith("I")):
         # Generate the wav -> MFC script
         print("Generating the wav -> MFC conversion script")
-        generateConversionList()
+        wavConvertFile = str.format("{0}{1}{2}", SCRIPT_DIR, "WAV_MFC_Conversion_List", SCRIPT_EXT)
+        generateConversionList(TRAINING_AUDIO_DIR, wavConvertFile)
         print("Completed")
 
         sleep(SLEEP_S)
@@ -430,14 +496,17 @@ while (not command.startswith("Q")):
 
         # Generate the classifier lists
         print("Generating lists for each classifier")
-        generateClassifierLists()
+        for ext in CLASSIFIER_EXTS:
+            ext = ext.lstrip('.').upper()
+            listFile = str.format("{0}{1}_Training_List.scp", SCRIPT_DIR, ext)
+            generateClassifierLists(TRAINING_AUDIO_DIR, listFile)
         print("Completed")
 
         sleep(SLEEP_S)
 
         # Generate MLF
         print("Generating MLF")
-        generateMLF()
+        generateMLF(TRAINING_AUDIO_DIR, PHONE_NO_SP)
         print("Completed")
 
         sleep(SLEEP_S)
@@ -546,13 +615,14 @@ while (not command.startswith("Q")):
 
             # Realign phonetic data with dictionary
             print("Realigning the training data")
-            performRealignment(ext, currentIteration)
+            # performRealignment(ext, currentIteration)
             print("Completed")
 
             sleep(SLEEP_S)
 
             # Re-estimation 7-9
-            for i in range(3):
+            for i in range(TRAINING_COUNT - currentIteration):
+
                 print(str.format("Performing Re-Estimation {0} for the {1} classifier", currentIteration+1, ext))
 
                 performReestimation(ext, currentIteration, True, True)
@@ -564,6 +634,15 @@ while (not command.startswith("Q")):
                 sleep(SLEEP_S)
 
     elif (command.startswith("E")):
+        # Generate the training wav -> MFC script
+        print("Generating the wav -> MFC conversion script")
+        wavConvertFile = str.format("{0}{1}{2}", SCRIPT_DIR, "WAV_MFC_EVAL_Conversion_List", SCRIPT_EXT)
+        generateConversionList(EVAL_AUDIO_DIR, wavConvertFile)
+        print("Completed")
+
+        sleep(SLEEP_S)
+
+
 
     else:
         print("Invalid option.")
