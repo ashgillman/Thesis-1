0073408952df17c43f49d3464ce20f2b74664119
TheMerovingian <bryan.quill@gmail.com>
2014-09-15 13:22:13 +1000

    Removed thesis

--- Classifiers/Classifier.m
+++ Classifiers/Classifier.m
@@ -1,14 +1,24 @@
 clear all
 
 addpath('../Voicebox/')
+WAV = 'wav';
 
 % HTK output varaibles
 STFT_OUTPUT_CODE = 9;
 NNMF_OUTPUT_CODE = 9;
 LPC_OUTPUT_CODE = 1;
-AUDIO_DIR = '../../Audio/';
-OUTPUT_DIR = '../ClassifierOutput/';
-WAV_FILES = dir([AUDIO_DIR, '*.wav']); %{'C3CA010A.wav', 'C3CA010B.wav', 'C3CA010C.wav'};
+
+OUTPUT_DIR = 'F:/Thesis/External/ClassifierTraining/Classifiers/';
+
+TRAINING_AUDIO_DIR = 'F:/Thesis/External/ConvertData/ThesisData/Desk/Testing/Development/';
+TRAINING_OUTPUT_DIR = strcat(OUTPUT_DIR, 'Training/');
+TRAINING_FILES = getAllFiles(TRAINING_AUDIO_DIR);
+
+%TRAINING_WAV_FILES = dir([TRAINING_AUDIO_DIR, '*.wav']); %{'C3CA010A.wav', 'C3CA010B.wav', 'C3CA010C.wav'};
+
+EVAL_AUDIO_DIR = 'F:/Thesis/External/ConvertData/ThesisData/Desk/Testing/Evaluation/';
+EVAL_OUTPUT_DIR = strcat(OUTPUT_DIR, 'Eval/');
+EVAL_FILES = getAllFiles(EVAL_AUDIO_DIR);
 
 % STFT Variables
 FRAME_SIZE = 0.02;      % In seconds
@@ -20,15 +30,26 @@ OVERLAP = 50;           % Percent overlap
 % LPC Variables
 FILTER_ORDER = 12;
 
-for n = 1:length(WAV_FILES)
-    wavFile = WAV_FILES(n).name;
-    wavLoc = [AUDIO_DIR, wavFile]
-    [audio, Fs] = audioread(wavLoc);
+for n = 1:length(EVAL_FILES)
+    file = EVAL_FILES{n};
+    
+    path = strsplit(file, '.');
+    
+    ext = path{2};
+    
+    splitpath = strsplit(path{1}, '\\');
+    name = splitpath{end};
+    
+    if ~strcmp(ext, WAV)
+        continue
+    end
+    
+    [audio, Fs] = audioread(file);
     
     %% STFT
     STFTData = STFTClassifier(audio, Fs, OVERLAP, FRAME_SIZE, NUMBER_OF_PEAKS);
     
-    stftHTKFile = strcat(OUTPUT_DIR, wavFile(1:end-4), '.stft');
+    stftHTKFile = strcat(EVAL_OUTPUT_DIR, name, '.stft');
     
     if exist(stftHTKFile, 'file')
         delete(stftHTKFile);
@@ -40,7 +61,7 @@ for n = 1:length(WAV_FILES)
     %% LPC
     LPCData = LPCClassifier(audio, Fs, FILTER_ORDER, FRAME_SIZE);
     
-    lpcHTKFile = strcat(OUTPUT_DIR, wavFile(1:end-4), '.lpc');
+    lpcHTKFile = strcat(EVAL_OUTPUT_DIR, name, '.lpc');
     
     if exist(lpcHTKFile, 'file')
         delete(lpcHTKFile);
